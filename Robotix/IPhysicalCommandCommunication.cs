d4eaa311f22d32781e203c3e7a9a8242366607e0
MONKCs studios <ape@monkcs.se>
2015-12-14 22:10:36 +0100

    First upload

---
+++ Robotix/IPhysicalCommandCommunication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Robotix.External;
+
+namespace Robotix
+{
+    /// <summary>
+    /// Handler for communication between the remote control software and GPIO logic
+    /// </summary>
+    public interface IPhysicalCommandCommunication
+    {
+        /// <summary>
+        /// State changed in communication with physical equipment
+        /// </summary>
+        event EventHandler StateChanged;
+
+        /// <summary>
+        /// Will execute the keystate according to instructions for that key
+        /// </summary>
+        /// <param name="keyToInvoke">The key to submit</param>
+        void Invoke(Key keyToInvoke);
+
+        /// <summary>
+        /// Will submit the object to the qualified handler. May not work properly. Do not use if possible
+        /// </summary>
+        /// <param name="rawData">The object to submit</param>
+        void Invoke(object rawData);
+
+        /// <summary>
+        /// Start the polling and <code>UpdateRunner()</code>
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stopping the background thread
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Dispose all resources
+        /// </summary>
+        void Dispose();
+    }
+}
---
+++ RobotixTools/External/Pin.cs
@@ -0,0 +1,85 @@
+﻿using RaspberryPiDotNet;
+using System;
+
+namespace Robotix.External
+{
+    /// <summary>
+    /// Containing a pinstate, when passed to a class attached to IPhysicalCommandCommunication
+    /// will apply to a physical GPIO pin.
+    /// </summary>
+    public class Pin : GpioItem, IDisposable
+    {
+        /// <summary>
+        /// The initial value for the pin
+        /// </summary>
+        protected bool InitialValue;
+
+        /// <summary>
+        /// The pin communicating with the memory
+        /// </summary>
+        public GPIOMem PhysicalPin { get; protected set; }
+        /// <summary>
+        /// The current state of the item
+        /// </summary>
+        public virtual PinState CurrentState { get; protected set; }
+        /// <summary>
+        /// True if the runtime should poll the pin for changes.
+        /// Turn it off to prevent lag on timecritic applications. 
+        /// If turned off you will not get a value from the <code>GetValue()</code> methods, you have to use the native library read method
+        /// </summary>
+        public bool AvalibleForPolling { get; set; } = true;
+
+        /// <summary>
+        /// Containing a keystate
+        /// </summary>
+        public Pin(GPIOPins pin, GPIODirection direction, bool initialValue) : this(pin, direction, initialValue, null) { }
+        /// <summary>
+        /// Containing a keystate
+        /// </summary>
+        public Pin(GPIOPins pin, GPIODirection direction, bool initialValue, string friendlyName)
+        {
+            PhysicalPin = new GPIOMem(pin, direction, initialValue);
+            FriendlyName = friendlyName;
+            InitialValue = initialValue;
+        }
+
+        /// <summary>
+        /// Update the current value for the pin. Returns true if value changed from last scan
+        /// </summary>
+        public bool ReadValue()
+        {
+            if (PhysicalPin != null)
+            {
+                PinState temp = CurrentState;
+                CurrentState = PhysicalPin.Read();
+                if (CurrentState != temp)
+                {
+                    JustChanged = true;
+                }
+                else
+                {
+                    JustChanged = false;
+                }
+            }
+            return JustChanged;
+        }
+
+        /// <summary>
+        /// Disposes all resources
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                if (PhysicalPin.PinDirection == GPIODirection.Out)
+                {
+                    PhysicalPin.Write(false);
+                }
+                if (!PhysicalPin.IsDisposed)
+                {
+                    PhysicalPin.Dispose();
+                }
+            } catch { }
+        }
+    }
+}
