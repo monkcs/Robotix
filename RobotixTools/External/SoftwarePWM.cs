d8c45f60903e21724d46c2fa40ca83c2211cd210
MONKCs studios <ape@monkcs.se>
2016-01-14 15:11:25 +0100

    Removed C# 6.0 features

--- RobotixTools/External/SoftwarePWM.cs
+++ RobotixTools/External/SoftwarePWM.cs
@@ -1,4 +1,4 @@
-﻿using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -110,4 +110,4 @@ namespace Robotix.External
             PhysicalPin.Write(InitialValue);
         }
     }
-}
+}*/
--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mcs /target:library /out:RobotixTools.dll -r:/../WiringPiSharp/WiringPiSharp.dll RobotixTools/*.cs RobotixTools/External/*.cs
-mcs /target:library /out:Robotix.dll -r:/../WiringPiSharp/WiringPiSharp.dll Robotix/*.cs
+cp ../WiringPiSharp/WiringPiSharp.dll WiringPiSharp.dll
+mcs /target:library /out:RobotixTools.dll -r:WiringPiSharp.dll RobotixTools/*.cs RobotixTools/External/*.cs
+mcs /target:library /out:Robotix.dll -r:WiringPiSharp.dll Robotix/*.cs
