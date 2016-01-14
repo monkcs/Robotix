8e67b432ea5ad38a440e79273e9f0e1c0461fba5
MONKCs studios <ape@monkcs.se>
2016-01-14 15:01:40 +0100

    Removed dublett

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+mcs /target:library /out:RobotixTools.dll -r:/../WiringPiSharp/WiringPiSharp.dll RobotixTools/*.cs RobotixTools/External/*.cs
+mcs /target:library /out:Robotix.dll -r:/../WiringPiSharp/WiringPiSharp.dll Robotix/*.cs
