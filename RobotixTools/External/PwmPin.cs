95785100b31d2c92d21ae9c01357deeb2da93457
MONKCs studios <ape@monkcs.se>
2016-01-24 23:02:49 +0100

    Added new class with constructor for pwm pin

---
+++ RobotixTools/External/PwmPin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WiringPiSharp;
+
+namespace Robotix.External
+{
+	/// <summary>
+	/// Containing a pinstate for a pwm pin, when passed to a class attached to IPhysicalCommandCommunication
+	/// will apply to a physical GPIO pin.
+	/// </summary>
+	public class PwmPin : DigitalPin, IDisposable
+	{
+		/// <summary>
+		/// Containing a pinstate for PWM pin
+		/// </summary>
+		/// <param name="pin">The pin communicating with the memory</param>
+		/// <param name="value"></param>
+		/// <param name="range"></param>
+		public PwmPin (WiringPi.WPiPinout pin, int value, int range)
+		{
+			WiringPiSharp.SoftPwm.SoftPwmCreate (pin, value, range);
+		}
+
+
+	}
+}
+
