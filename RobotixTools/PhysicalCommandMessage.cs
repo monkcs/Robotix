87c55695eeab46d309135f2386803d38c452c6a2
MONKCs studios <ape@monkcs.se>
2016-01-29 14:35:48 +0100

    Added return message class to the console from robotix

---
+++ RobotixTools/PhysicalCommandMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Robotix
+{
+	public class PhysicalCommandMessage
+	{
+		public MessageType Message {get;}
+		public Action WriteMessage;
+		public string Identifier { get;}
+		public Type TypeOfObject;
+		public object ValueObject;
+
+		public PhysicalCommandMessage (MessageType message,Action writeMessage,string identifier, Type typeOfObject, Object valueObject)
+		{
+			Message = message;
+			WriteMessage = writeMessage;
+			Identifier = identifier;
+			TypeOfObject = typeOfObject;
+			ValueObject = valueObject;
+		}
+		enum MessageType
+		{
+			/// <summary>
+			/// Information to show in the console
+			/// </summary>
+			Information,
+			/// <summary>
+			/// Value from robotix object
+			/// </summary>
+			RawData
+
+		}
+	}
+}
+
