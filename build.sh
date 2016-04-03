d9954a9da6191893d574d39bf8f66085679c9b5e
MONKCs studios <ape@monkcs.se>
2016-04-03 17:35:14 +0200

    Added xml documentation

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cd WiringPiSharp
 ./build.sh
 cd ..
 cp WiringPiSharp/WiringPiSharp.dll WiringPiSharp.dll
-mcs -target:library -out:Robotix.dll -r:WiringPiSharp.dll Robotix/*.cs Robotix/External/*.cs
+cp WiringPiSharp/WiringPiSharp.xml WiringPiSharp.xml
+mcs -target:library -out:Robotix.dll -r:WiringPiSharp.dll Robotix/*.cs Robotix/External/*.cs -doc:Robotix.xml
