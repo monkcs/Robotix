684e4420554f5cdf90d6f66684d37d3b72d354c9
MONKCs studios <ape@monkcs.se>
2016-06-25 19:50:49 +0200

    Added copy to new folder

--- build.cmd
+++ build.cmd
@@ -6,9 +6,13 @@ cd tools
 SET path=%CD%
 cd %currentpath%
 
+mkdir robotix-compiled
 cd WiringPiSharp
 build.cmd
 cd ..
-copy WiringPiSharp\WiringPiSharp.dll WiringPiSharp.dll
-copy WiringPiSharp\WiringPiSharp.xml WiringPiSharp.xml
+copy WiringPiSharp\WiringPiSharp.dll robotix-compiled\WiringPiSharp.dll
+copy WiringPiSharp\WiringPiSharp.xml robotix-compiled\WiringPiSharp.xml
 csc /target:library /out:Robotix.dll /r:WiringPiSharp.dll Robotix\*.cs Robotix\External\*.cs /doc:Robotix.xml
+
+copy Robotix.dll robotix-compiled\Robotix.dll
+copy Robotix.xml robotix-compiled\Robotix.xml
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+mkdir robotix-compiled
 cd WiringPiSharp
 ./build.sh
 cd ..
-cp WiringPiSharp/WiringPiSharp.dll WiringPiSharp.dll
-cp WiringPiSharp/WiringPiSharp.xml WiringPiSharp.xml
+cp WiringPiSharp/WiringPiSharp.dll robotix-compiled/WiringPiSharp.dll
+cp WiringPiSharp/WiringPiSharp.xml robotix-compiled/WiringPiSharp.xml
 mcs -target:library -out:Robotix.dll -r:WiringPiSharp.dll Robotix/*.cs Robotix/External/*.cs -doc:Robotix.xml
+cp Robotix.dll robotix-compiled/Robotix.dll
+cp Robotix.xml robotix-compiled/Robotix.xml
